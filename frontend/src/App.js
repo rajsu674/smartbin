c9c3fb91faaa71fe29430d0f7297b7970d19b2cf
Rajeshkumar Sundaram <rajeshkumar.sundaram@cybercom.com>
2018-11-13 09:07:29 +0100

    App text color added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
+  render() {    
     return (
       <div>
         <header className="App-header">
-          <p style = { {color: '#000'}}>Practicing AWS in React</p>
+          <p className = 'App-text'>Practicing AWS in React</p>
         </header>
       </div>
     );
