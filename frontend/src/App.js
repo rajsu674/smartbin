ae419c7d8080c8bf9c1b11b92d834340444fe0fc
Rajeshkumar Sundaram <rajeshkumar.sundaram@cybercom.com>
2018-11-13 09:01:31 +0100

    First commit with initial React files

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <p style = { {color: '#000'}}>Practicing AWS in React</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
